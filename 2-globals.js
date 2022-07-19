28c4edcbf71a313f80f83b3f9a6c26c0ccc2883d
Emmanuel Zelarayan <emmavz@live.com>
2022-07-19 16:22:40 -0300

    intro and globals

---
+++ 2-globals.js
@@ -0,0 +1,13 @@
+// GLOBALS - NO WINDOW !!!!
+
+// __dirname  - path to current directory
+// __dilename - file name
+// require    - function to use modules (CommonJS)
+// module     - info about current module (file)
+// process    - info about env where the program is being executed
+
+console.log(__dirname);
+
+setInterval(() => {
+  console.log("Hello world");
+}, 1000);
