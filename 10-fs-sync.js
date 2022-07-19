a4c3e63633d3e61be2a280cd8ae4412352db656e
Emmanuel Zelarayan <emmavz@live.com>
2022-07-19 19:30:50 -0300

    os, path, fs sync and async modules

---
+++ 10-fs-sync.js
@@ -0,0 +1,16 @@
+const { readFileSync, writeFileSync } = require("fs");
+// const fs = require('fs');
+console.log('Start');
+
+const first = readFileSync("./content/first.txt", "utf-8");
+const second = readFileSync("./content/second.txt", "utf-8");
+// console.log(first, second);
+
+writeFileSync(
+    "./content/result-sync.txt",
+     `Here is the result : ${first}, ${second}`,
+    //  {flag: 'a'} // append
+);
+
+console.log('Done with this task');
+console.log('Starting the next one');
